9baa78b1c402922d52e9f2ebf070f1acf16d1207
kawinnat09 <Kwinnathmayura@gmail.com>
2022-11-01 21:23:33 +0700

    first commit

---
+++ src/main/kotlin/ IfelseStatement2.kt
@@ -0,0 +1,16 @@
+fun fElseStatement(){
+    var balance = 30_000
+    val withdraw = 2_500
+
+    if(balance<withdraw){
+        println("ยอดเงินคงเหลือไม่เพียงพอ")
+    }else if(withdraw>20_000){
+        println("ถอนไม่เกิน 20_00 บาท")
+    }else if (withdraw % 100 != 0){
+        println("ต้องเป็นจำนวนเต็มร้อยเท่านั้น")
+    }
+    else {
+        balance -= withdraw //balance = balance - withdraw
+        print("ยอดเงินคงเหลือ: $balance บาท")
+    }
+}
